f12a1cfa2ba2752eec12d53840c170767c18301e
Konstantin Dankov <konstantin.dankov@gmail.com>
2013-02-25 22:06:03 +0200

    Updated SASS compilation to make it work with FireSASS plugin

--- config.rb
+++ config.rb
@@ -6,11 +6,12 @@ http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
-javascripts_dir = "images"
+javascripts_dir = "scripts"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :compressed
+expanded = :expanded
 # or :nested or :compact or :compressed
+sass_options = {:debug_info => true}
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
