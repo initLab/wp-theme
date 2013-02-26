4006aa0105675ce1f145effa3f41e283f85253e6
Konstantin Dankov <konstantin.dankov@gmail.com>
2013-02-26 14:03:19 +0200

    Page template update

--- page.php
+++ page.php
@@ -1,7 +1,16 @@
 <?php get_header(); ?>
 
 	<?php if (have_posts()) { while (have_posts()) { the_post(); ?>
-		<?php include 'loop/default.php'; ?>
+
+		<article class="page">
+			<header>
+				<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+			</header>
+			<div class="content">
+				<?php the_content(); ?>
+			</div>
+		</article>
+
 	<?php } } ?>
 
 <?php get_footer(); ?>
