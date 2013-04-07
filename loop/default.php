7dd175a311a1471708b3af6063009778020cef0a
Konstantin Dankov <konstantin.dankov@gmail.com>
2013-04-08 00:05:32 +0300

    fixed date generation

--- loop/default.php
+++ loop/default.php
@@ -1,7 +1,7 @@
 <article class="entry">
 	<header>
 		<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-		<p class="meta"><?php the_time('d M Y'); ?></p>
+		<p class="meta"><?php the_date(); ?></p>
 	</header>
 	<div class="content">
 		<?php the_excerpt(); ?>
