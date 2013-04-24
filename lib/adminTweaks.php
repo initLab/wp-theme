3fc5974b74682b2691ed4e736c4ee4ba4372ff67
Konstantin Dankov <konstantin.dankov@gmail.com>
2013-04-24 19:22:51 +0300

    Hidden the option to change themes

--- lib/adminTweaks.php
+++ lib/adminTweaks.php
@@ -3,6 +3,14 @@
 	if(is_admin())
 	{
 
+		// Hide the option of choosing a different theme
+		add_action('admin_init', 'my_remove_menu_elements', 102);
+		function my_remove_menu_elements()
+		{
+			remove_submenu_page( 'themes.php', 'themes.php' );
+		}
+
+		
 	}
 
 ?>
