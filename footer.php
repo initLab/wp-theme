ba78a4598a500d973f5219c5c1d7ac2a7b0a008f
Konstantin Dankov <konstantin.dankov@gmail.com>
2013-04-24 19:21:20 +0300

    Added Google Analytics code

--- footer.php
+++ footer.php
@@ -18,12 +18,22 @@
 	<script src="<?php bloginfo('template_url'); ?>/scripts/foundation/jquery.customforms.js"></script>
 	<script src="<?php bloginfo('template_url'); ?>/scripts/foundation/jquery.placeholder.min.js"></script>
 	<script src="<?php bloginfo('template_url'); ?>/scripts/foundation/jquery.tooltips.js"></script>
-
 	*/ ?>
+
 	<script src="<?php bloginfo('template_url'); ?>/scripts/jquery.tweet.js"></script>
 	<script src="<?php bloginfo('template_url'); ?>/scripts/jflickrfeed.min.js"></script>
 	<script src="<?php bloginfo('template_url'); ?>/scripts/app.js"></script>
+	<script type="text/javascript">
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-18158223-1']);
+		_gaq.push(['_trackPageview']);
 
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+	</script>
 	<?php wp_footer(); ?>
 </body>
 </html>
