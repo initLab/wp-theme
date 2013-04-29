9cdc2a8fc14363ddacfc7f7fb1cef12e8135d1b8
Konstantin Dankov <konstantin.dankov@gmail.com>
2013-04-29 13:00:47 +0300

    Fixed the header title link on the homepage

--- breadcrumbs.php
+++ breadcrumbs.php
@@ -27,7 +27,7 @@
 
 		foreach($link as $l)
 		{
-			if (is_front_page())		{ echo '<h2>&nbsp;/ <a href="'.$l[1].'">Hackerspace in Sofia</a></h2>';	}
+			if (is_front_page())		{ echo '<h2>&nbsp;/ Hackerspace in Sofia</h2>';	}
 			elseif ($l[0]=='Any')		{ echo '<h2>&nbsp;/ <a href="'.$l[1].'">Search</a></h2>';		}
 			elseif ($l[0]=='Faq')		{ echo '<h2>&nbsp;/ <a href="'.$l[1].'">ЧЗВ</a></h2>';			}
 			elseif ($l[0]=='Events')	{ echo '<h2>&nbsp;/ <a href="'.$l[1].'">Събития</a></h2>';		}
