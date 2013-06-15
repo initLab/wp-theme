3af9d91769beddcacdab19d91f6a8d1c06705292
Konstantin Dankov <konstantin.dankov@gmail.com>
2013-06-15 17:12:47 +0300

    white space tweaks

--- lib/relations.php
+++ lib/relations.php
@@ -1,16 +1,20 @@
 <?php
 
-p2p_register_connection_type( array(
-    'name' => 'multiple_speakers',
-    'from' => 'courses',
-    'to' => 'user'
-) );
+	p2p_register_connection_type( 
+		array(
+			'name' => 'multiple_speakers',
+			'from' => 'courses',
+			'to	' => 'user'
+		)
+	);
 
-p2p_register_connection_type( array(
-    'name' => 'multiple_authors',
-    'from' => 'tribe_events',
-    'to' => 'user'
-) );
+	p2p_register_connection_type(
+		array(
+			'name' => 'multiple_authors',
+			'from' => 'tribe_events',
+			'to' => 'user'
+		)
+	);
 
 
 ?>
