efcc0ee8df46f84402c0f6a66e70169a1864da9d
Konstantin Dankov <konstantin.dankov@gmail.com>
2013-09-06 00:30:12 +0300

    Small visual update

--- archive-news.php
+++ archive-news.php
@@ -1,19 +1,8 @@
 <?php get_header(); ?>
 
-	<div class="list-wrapper">
-		<?php if (have_posts()) { while (have_posts()) { the_post(); ?>
-			<?php include 'loop/news.php'; ?>
-		<?php } } ?>
-	</div>
-	<aside>
-		<section>
-			<header>
-				<h3>Последно в Twitter</h3>
-			</header>
-			<div id="tweets" class="messages">
-			</div>
-		</section>
-	</aside>
+	<?php if (have_posts()) { while (have_posts()) { the_post(); ?>
+		<?php include 'loop/news.php'; ?>
+	<?php } } ?>
 
 <?php get_footer(); ?>
 
