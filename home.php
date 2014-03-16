d17aea3f7634c66d1f6d9f06e81101f830ca2281
Vladimir Vassilev <vlood@shtrak.eu>
2014-03-16 14:15:22 +0200

    активиране на sidebar 2 в началната страница

--- home.php
+++ home.php
@@ -9,11 +9,9 @@
 	<div class="row">
 		<?php dynamic_sidebar("Homepage - Row 1"); ?>
 	</div>
-<?php /* ?>
 	<div class="row" id="site-sections">
 		<?php dynamic_sidebar("Homepage - Row 2"); ?>
 	</div>
-<?php */ ?>
 	<div class="row" id="events">
 		<?php dynamic_sidebar("Homepage - Row 3"); ?>
 	</div>
